254f03ca7af3811a368994ab96126d62801cf1a2
kkt9102 <kkt9102@gmail.com>
2022-02-04 08:41:27 +0900

    커피리스트 작업

--- src/components/coffee_list.js
+++ src/components/coffee_list.js
@@ -1,6 +1,10 @@
 import { Link } from "react-scroll";
 import '../resources/css/style.css';
 
+// img
+import miniLogo from '../resources/img/coffee_list_logo.svg';
+import panama from '../resources/img/Panama Boquete.jpg';
+
 const CoffeeList = () => {
 
     return (
@@ -8,7 +12,13 @@ const CoffeeList = () => {
             <li className="coffee_item">
                 <Link to="">
                     <div className="img_box">
-                        <img></img>
+                        <img src={panama} alt=""></img>
+                        <div className="logo">
+                            <img src={miniLogo} alt="리저브 로고"></img>
+                        </div>
+                        <div className="coffee_name">
+                            <span></span>
+                        </div>
                     </div>
                 </Link>
             </li>
--- src/components/reserve_section_5.js
+++ src/components/reserve_section_5.js
@@ -11,8 +11,9 @@ const ReserveSC5 = () => {
         <>
             <section id="section_5" className="section re_sc_5">
                 <div className="con">
-                    <div className="section_tit flex flex_jc_c">
+                    <div className="section_tit flex flex_jc_c flex_wrap_wrap">
                         <img src={S_5_tit} alt="리저브 매장 RESERVE™ LOCATION"></img>
+                        <div className="sub_tit flex flex_jc_c">전국의 스타벅스 리저브™ 매장 <span className="mark_count"></span> 곳을 확인하세요</div>
                     </div>
                     <div className="map_box">
                         <Map/>
