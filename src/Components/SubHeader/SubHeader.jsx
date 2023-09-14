4456928483a55251af36fe3861ac76e4c00a8998
kkt9102 <kkt9102@gmail.com>
2023-09-14 23:08:07 +0900

    feat : data 추가

--- src/Components/SubHeader/SubHeader.jsx
+++ src/Components/SubHeader/SubHeader.jsx
@@ -1,9 +1,9 @@
 const SubHeader = ({ko_name,en_name}) => {
   return(
-    <>
+    <div className="sub_header">
       <div className="ko_name">{ko_name}</div>
       <div className="en_name">{en_name}</div>
-    </>
+    </div>
   )
 }
 
