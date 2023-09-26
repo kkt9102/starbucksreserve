8b361e91c818bd80ff1e31fcc5314293505c7790
kkt9102 <kkt9102@gmail.com>
2023-09-26 18:43:26 +0900

    feat : 상세페이지 작업중

--- src/Pages/CoffeeProducts/CoffeeProducts.jsx
+++ src/Pages/CoffeeProducts/CoffeeProducts.jsx
@@ -5,6 +5,8 @@ import Footer from '../../Components/Footer';
 
 import CoffeeInfo from '../../Data/CoffeeDetail.json';
 
+import { ReactComponent as StarScore } from '../../Assets/Images/Star_icon.svg';
+
 const CoffeeProducts = () => {
   const { coffee } = useParams();
 
@@ -19,12 +21,31 @@ const CoffeeProducts = () => {
           <img src={`${coffeeInfo[0].img}`} alt={coffeeInfo[0].ko_name}/>
         </div>
         <div className='coffee_info'>
-          <div className='coffee_origin flex'>
-            <span>원산지 : </span>
-            <span>{coffeeInfo[0].origin}</span>
+          <div className='coffee_origin deco_txt flex'>
+            <span className='key_title'>원산지&nbsp;:</span>
+            <span className='origin_location'>&nbsp;{coffeeInfo[0].origin}</span>
           </div>
           <div className='coffee_info_chart'>
-            
+            <div className='tasting flex flex_dir_c'>
+              <div className='key'>Tasting Notes</div>
+              <div className='value'>{coffeeInfo[0].tasting_notes}</div>
+            </div>
+            <div className='enjoy flex flex_dir_c'>
+              <div className='key'>Enjoy With</div>
+              <div className='value'>{coffeeInfo[0].enjoy_with}</div>
+            </div>
+            <div className='methods flex flex_dir_c'>
+              <div className='key'>Processing Method</div>
+              <div className='value'>{coffeeInfo[0].processing_method}</div>
+            </div>
+            <div className='body_score flex flex_dir_c'>
+              <div className='key'>Body</div>
+              <div className='value'><StarScore/></div>
+            </div>
+            <div className='acidity_score flex flex_dir_c'>
+              <div className='key'>Acidity</div>
+              <div className='value'><StarScore/></div>
+            </div>
           </div>
         </div>
       </div>
