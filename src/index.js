0002c9c086d0b092ce6ff145b380dd64fc633e63
kkt9102 <kkt9102@gmail.com>
2023-10-30 18:46:59 +0900

    feat : router change

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './Assets/Styles/_index.css';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </RecoilRoot>
