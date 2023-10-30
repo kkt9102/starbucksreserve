e99c9def49b628651ea8c002bd860fb81f311772
kkt9102 <kkt9102@gmail.com>
2023-10-30 18:50:24 +0900

    feat : router option add

--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      {/* <HashRouter basename={process.env.PUBLIC_URL}> */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </RecoilRoot>
   </React.StrictMode>
 );
